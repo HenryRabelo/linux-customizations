a341825d2956e16e37be03200dd5eea497ccf113
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2024-07-01 23:46:24 -0300

    Update shell_prompts.sh

--- extras/shell_prompts.sh
+++ extras/shell_prompts.sh
@@ -1,12 +1,17 @@
-# Debian ZSH Prompt
+## ZSH Prompts
+# Debian ZSH Prompt (Kali One-Liner Style)
 PROMPT='${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))}%B%F{%(!.red.green)}%n@%m%b%F{%(!.green.cyan)}-[%B%F{reset}%(6~.%-1~/…/%4~.%5~)%b%F{%(!.green.cyan)}]%F{reset}%# '
 
-# Fedora-Style Debian ZSH Prompt
-PROMPT='${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))}[%B%F{%(!.red.green)}%n@%m%b%F{cyan} %(5~.%-2~/…/%1~.%4~)%F{reset}]%# '
+# Debian ZSH Prompt (Fedora Style)
+PROMPT='${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))}[%F{%(!.red.green)}%n@%m%F{cyan} %(5~.%-2~/…/%1~.%4~)%F{reset}]%# '
 
 # Fedora Cyan ZSH Prompt
 PROMPT='[%F{%(!.red.cyan)}%n@%m%F{reset} %(5~.%-2~/…/%1~.%4~)]%# '
 
+# openSUSE Green ZSH Prompt (Kali One-Liner Alt.)
+PROMPT='%F{%(!.red.green)}%n@%m%F{%(!.green.cyan)}[%F{reset}%(5~.%-2~/…/%1~.%4~)%F{%(!.green.cyan)}]%F{reset}%(!.#.>)'
+
+## Bash Prompts
 # openSUSE Green Bash Prompt
 PS1='\[\e[32m\]\[$(ppwd)\]\u@\h\[\e[m\]:\w>'
 
