69453e29414b2ee40900ed7245debd6373617401
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2026-05-15 11:43:22 -0300

    Update snap-clean quoting

--- snap-clean.sh
+++ snap-clean.sh
@@ -7,12 +7,13 @@ Clean() {
     exit 1
   fi
   
-  local TOTAL=$(snap list --all | grep disabled | wc -l)
+  local -r TOTAL="$(snap list --all | grep disabled --count)"
   
   for INT in $(seq 1 "$TOTAL"); do
-    local PKGS=$(snap list --all | grep disabled)
-    local NAME=$(echo $PKGS | awk 'NR==1 {print $1}')
-    local REV=$(echo $PKGS | awk 'NR==1 {print $3}')
+    local PKGS NAME REV
+    PKGS="$(snap list --all | grep disabled)"
+    NAME="$(echo "$PKGS" | awk 'NR==1 {print $1}')"
+    REV="$(echo "$PKGS" | awk 'NR==1 {print $3}')"
     snap remove "--revision=$REV" "$NAME"
   done
 
